e50086a5d9f110df87e7d0dedbffbc1183334896
RealHenryGX <realhenrygx@users.noreply.github.com>
2026-08-20 22:33:51 +0800

    Add path_umount backport (5.9) to fix-susfs-4.9.py - resolves KSU umount link error on 4.9

--- scripts/fix-susfs-4.9.py
+++ scripts/fix-susfs-4.9.py
@@ -108,8 +108,56 @@ def patch_selinux():
     open(p, 'w', encoding='utf-8').write(s)
     print('patched', p)
 
+def patch_namespace():
+    # Backport path_umount from 5.9 (KernelSU doc: required for pre-5.9 kernels)
+    p = 'fs/namespace.c'
+    s = open(p, encoding='utf-8').read()
+    if 'int path_umount(struct path *path, int flags)' in s:
+        print('skip', p, '(path_umount already present)')
+        return
+    block = (
+        '\n'
+        'static int can_umount(const struct path *path, int flags)\n'
+        '{\n'
+        '\tstruct mount *mnt = real_mount(path->mnt);\n'
+        '\n'
+        '\tif (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))\n'
+        '\t\treturn -EINVAL;\n'
+        '\tif (!may_mount())\n'
+        '\t\treturn -EPERM;\n'
+        '\tif (path->dentry != path->mnt->mnt_root)\n'
+        '\t\treturn -EINVAL;\n'
+        '\tif (!check_mnt(mnt))\n'
+        '\t\treturn -EINVAL;\n'
+        '\tif (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */\n'
+        '\t\treturn -EINVAL;\n'
+        '\tif (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))\n'
+        '\t\treturn -EPERM;\n'
+        '\treturn 0;\n'
+        '}\n'
+        '\n'
+        'int path_umount(struct path *path, int flags)\n'
+        '{\n'
+        '\tstruct mount *mnt = real_mount(path->mnt);\n'
+        '\tint ret;\n'
+        '\n'
+        '\tret = can_umount(path, flags);\n'
+        '\tif (!ret)\n'
+        '\t\tret = do_umount(mnt, flags);\n'
+        '\n'
+        '\t/* we mustn\'t call path_put() as that would clear mnt_expiry_mark */\n'
+        '\tdput(path->dentry);\n'
+        '\tmntput_no_expire(mnt);\n'
+        '\treturn ret;\n'
+        '}\n'
+    )
+    s = s.rstrip() + block
+    open(p, 'w', encoding='utf-8').write(s)
+    print('patched', p, '(path_umount backport appended)')
+
 if __name__ == '__main__':
     assert os.path.isdir('fs') and os.path.isdir('KernelSU'), 'run from kernel source root'
     patch_include()
     patch_selinux()
+    patch_namespace()
     print('fix-susfs-4.9 done')
